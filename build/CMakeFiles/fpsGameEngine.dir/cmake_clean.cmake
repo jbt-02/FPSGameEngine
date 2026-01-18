b0a24e48b97940db0f8dc3930e51d60dd69833f3
jbt-02 <trachsjb@gmail.com>
2026-01-18 13:25:04 -0500

    We finally have a window

---
+++ build/CMakeFiles/fpsGameEngine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fpsGameEngine.dir/src/glad.c.o"
+  "CMakeFiles/fpsGameEngine.dir/src/glad.c.o.d"
+  "CMakeFiles/fpsGameEngine.dir/src/main.cpp.o"
+  "CMakeFiles/fpsGameEngine.dir/src/main.cpp.o.d"
+  "fpsGameEngine"
+  "fpsGameEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/fpsGameEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,46 @@
+#include "config.h"
 #include <iostream>
-#include <include/glad/glad.h>
-#include <glfw3.h>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);   
 
-int main(void){
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    
-    
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    
+
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
-        std::cout << "Failed to initialize GLAD" << std::endl;
+    /* Initialize GLAD - THIS IS REQUIRED */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
         return -1;
-    }  
+    }
 
-    glViewport(0, 0, 800, 600);
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
-
-    while(!glfwWindowShouldClose(window)){
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-        glfwPollEvents();    
+
+        /* Poll for and process events */
+        glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height){
-    glViewport(0, 0, width, height);
 }
